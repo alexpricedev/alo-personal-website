448a98ce07fd768ad5d80399de235f3d2fa838e3
Annette Lyn O'Neil <github@alexprice.dev>
2026-06-30 10:41:36 +0100

    chore: remove orphaned components and flash utils after demo-page removal

--- src/server/components/badge.tsx
+++
@@ -1,8 +0,0 @@
-interface BadgeProps {
-  variant: "admin" | "user";
-  children: React.ReactNode;
-}
-
-export const Badge = ({ variant, children }: BadgeProps) => (
-  <span className={`badge badge-${variant}`}>{children}</span>
-);
